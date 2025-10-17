5d8c07afa08d085b3495eb7f36f38a4c39ac485e
wijsmane <wijsmane@tcd.ie>
2025-10-17 16:39:26 +0100

    debugging and  add stark to conan

--- conanfile.py
+++ conanfile.py
@@ -164,6 +164,7 @@ class Xrpl(ConanFile):
             'xrpl.libpb',
             'ed25519',
             'secp256k1',
+            'stark'
         ]
         # TODO: Fix the protobufs to include each other relative to
         # `include/`, not `include/ripple/proto/`.
